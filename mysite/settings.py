1e2d9f023fe561a40840dc350d3661b00aa980ce
yomi8802 <morita8002@gmail.com>
2023-08-08 12:43:25 +0900

    Revert "url変更"
    This reverts commit df82ffe361fa1142f9e6fe62bfdd835afc993d2f.

--- mysite/settings.py
+++ mysite/settings.py
@@ -3,6 +3,10 @@ import environ
 from decouple import config
 import dj_database_url
 from pathlib import Path
+from django.core.management.utils import get_random_secret_key
+
+SECRET_KEY = get_random_secret_key()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 
