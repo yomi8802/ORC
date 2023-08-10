861fb50387090209386d440d82e2be1a42990abc
yomi8802 <morita8002@gmail.com>
2023-08-10 19:11:43 +0900

    Revert "staaticfiles修正"
    This reverts commit bffee40a0396905584021553f0693fc903976d6d.

--- mysite/settings.py
+++ mysite/settings.py
@@ -144,7 +144,7 @@ USE_TZ = True
 AUTH_USER_MODEL = 'accounts.CustomUser'
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATIC_DIRS = (os.path.join(BASE_DIR, 'static'),)
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
