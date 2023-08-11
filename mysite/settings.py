e21cb51e6138e6c40dcc9479e6df0ad8b62b8feb
yomi8802 <morita8002@gmail.com>
2023-08-11 22:48:01 +0900

    media_root表記変更

--- mysite/settings.py
+++ mysite/settings.py
@@ -149,7 +149,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_ROOT = BASE_DIR / "media"
 
 LOGIN_URL = '/'
 LOGIN_REDIRECT_URL = '/orc/index'
